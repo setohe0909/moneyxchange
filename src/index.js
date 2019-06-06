d28c429302d2f73d55d39bcdb2795f35efc991d7
sebastian tobon <setohe.09@gmail.com>
2019-06-06 16:20:35 -0500

    init setup  react + parcel + sass

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './assets/scss/app.scss';
+import Header from './components/Header';
+
+class HelloMessage extends React.Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        <div className="container">
+          <h1>Hello {this.props.name}</h1>
+        </div>
+      </div>
+    );
+  }
+}
+
+let App = document.getElementById('app');
+
+ReactDOM.render(<HelloMessage name="seto" />, App);
